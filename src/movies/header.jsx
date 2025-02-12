e813f193ff48b765211c3215acd12cf7bbc745f3
venskie03 <kevinkyleganados03@gmail.com>
2025-02-13 03:29:27 +0800

    update contact

--- src/movies/header.jsx
+++ src/movies/header.jsx
@@ -90,7 +90,7 @@ export default function Header() {
                 <a href="/ApiDocs" className="text-white border-b-2 border-transparent transition ease-in-out duration-300 hover:border-red-500 hover:text-red-500 hover:border-opacity-100 hover:delay-100">API Docs</a>
               </li>
               <li className="p-3">
-                <a href="https://kevinkyleganados.netlify.app/" target="_blank" className="text-white border-b-2 border-transparent transition ease-in-out duration-300 hover:border-red-500 hover:text-red-500 hover:border-opacity-100 hover:delay-100">Contact Me!</a>
+                <a href="https://www.facebook.com/kyle.ganados.3" target="_blank" className="text-white border-b-2 border-transparent transition ease-in-out duration-300 hover:border-red-500 hover:text-red-500 hover:border-opacity-100 hover:delay-100">Contact Me!</a>
               </li>
          
             </ul>
